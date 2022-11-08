b9870990d45ddd9c6515a0db79b336ef64a51e37
Ryan <rylarson17@gmail.com>
2022-11-07 22:49:29 -0700

    Updated functions to store text entries to local storage and display saved text upon page refresh

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,7 @@ var hourEl = $(".time-block");
 var date = dayjs().format("dddd, MMMM Do");
 var currentHour = dayjs().format("H");
 var buttonEl = $(".btn");
-console.log(buttonEl.eq(0).parent().get(0).id);
+var textEl = $(".description");
 
 $(function () {
   // Added the current date to the header of the calendar
@@ -11,7 +11,6 @@ $(function () {
 
   // Added function to compare current time to time block and add class for past, present or future
   hourEl.each(function (i) {
-    console.log(hourEl.eq(i).data("hour"));
     if (hourEl.eq(i).data("hour") < currentHour) {
       hourEl.eq(i).addClass("past");
     } else if (hourEl.eq(i).data("hour") == currentHour) {
@@ -21,19 +20,18 @@ $(function () {
     }
   });
 
-  buttonEl.click(function () {
+  // Added event listener for each button that will store the text content of the textarea form with the ket name equal to the time block id hour-"i"
+  buttonEl.click(function (event) {
+    event.preventDefault();
     clickedId = $(this).parent().get(0).id;
-    // savedText =
+    savedText = $(this).parent().children().eq(1).val();
+    localStorage.setItem(clickedId, savedText);
   });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // For each text area element get the stored key value of each id number and add it as the text value for each text area input
+  textEl.each(function (i) {
+    var index = i + 9;
+    var text = localStorage.getItem("hour-" + index);
+    $(this).val(text);
+  });
 });
